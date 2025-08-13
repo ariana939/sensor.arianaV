cd648b520f1615dca486f1df5528012b0f59f6d9
Ariana Villa <arianavilla4817@gmail.com>
2025-08-13 10:04:25 -0300

    usamos el simluador postman, agregamos methodos a la ruta,(agregamos una ruta para devolver un post)

--- servidor/app.py
+++ servidor/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/sensor", methods=['POST'])
+def sensor():
+    nombre = request.json["nombre"]
+    valor =  request.json ["valor"]  
+    print(f"el nombre es {nombre} y el valor es {valor}")
+    return "ok"
